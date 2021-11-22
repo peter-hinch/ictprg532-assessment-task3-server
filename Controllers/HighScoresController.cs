1965bb80370406bfd3041d10ebcd0d66c6a4cb73
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-22 17:10:05 +1100

    Add HighScoreController via code generation.

---
+++ Controllers/HighScoresController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AssessmentTask3Server.DbContexts;
+using AssessmentTask3Server.Models;
+
+// This controller is generated automatically via the following process:
+// 1. Right click on Controllers folder -> 'Add' -> 'New Scaffolded Item...'
+// 2. Select 'API Controller with actions, using Entity Framework'
+// 3. Specify Model class, Data context class and Controller name
+// 4. Allow the generation process to complete. May need a 2nd atempt.
+namespace AssessmentTask3Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HighScoresController : ControllerBase
+    {
+        private readonly HighScoreDbContext _context;
+
+        public HighScoresController(HighScoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/HighScores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HighScore>>> GetHighScores()
+        {
+            return await _context.HighScores.ToListAsync();
+        }
+
+        // GET: api/HighScores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HighScore>> GetHighScore(long id)
+        {
+            var highScore = await _context.HighScores.FindAsync(id);
+
+            if (highScore == null)
+            {
+                return NotFound();
+            }
+
+            return highScore;
+        }
+
+        // PUT: api/HighScores/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHighScore(long id, HighScore highScore)
+        {
+            if (id != highScore.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(highScore).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HighScoreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/HighScores
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<HighScore>> PostHighScore(HighScore highScore)
+        {
+            _context.HighScores.Add(highScore);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHighScore", new { id = highScore.Id }, highScore);
+        }
+
+        // DELETE: api/HighScores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHighScore(long id)
+        {
+            var highScore = await _context.HighScores.FindAsync(id);
+            if (highScore == null)
+            {
+                return NotFound();
+            }
+
+            _context.HighScores.Remove(highScore);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HighScoreExists(long id)
+        {
+            return _context.HighScores.Any(e => e.Id == id);
+        }
+    }
+}
