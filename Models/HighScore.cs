a8a5a51098c325a5bdf6bd7f1fb29734ed437f36
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-22 13:18:44 +1100

    Create model for high score object.

---
+++ Models/HighScore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssessmentTask3Server.Models
+{
+    public class HighScore
+    {
+        public int Score { get; set; }
+        public DateTime Date { get; set }
+    }
+}
